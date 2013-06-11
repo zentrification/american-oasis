aa7b7e2841ca00b150d504a0693dab12fbc0a1e1
Chris Dahms <zentrification@gmail.com>
2013-06-11 15:47:01 -0400

    refactored chapter locations, few css fixes

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  slim :'chapters/home'
+  #slim :'chapters/home'
+  slim :'chapters/chapter1'
 end
 
 get '/chapters/:page' do
-  page = 'chapters/' + params[:page]
+  page = 'chapters/chapter' + params[:page]
   slim page.to_sym
 end
