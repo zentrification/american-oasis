b8019c26b9e250d2deed1b762de4261549784f66
Chris Dahms <zentrification@gmail.com>
2013-07-05 18:25:18 -0400

    setup introduction page, video and overlay

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require 'sinatra'
 
-
-get '/' do
-  @cloudfront = 'http://d2sc30funx5qzm.cloudfront.net'
-  #slim :'chapters/home'
-  slim :'chapters/chapter1'
-end
-
 get '/chapters/:page' do
   @cloudfront = 'http://d2sc30funx5qzm.cloudfront.net'
+  @cloudfront = ''
   @page = params[:page]
   file_location = 'chapters/chapter' + @page
   slim file_location.to_sym
+end
+
+get '/*' do
+  @cloudfront = 'http://d2sc30funx5qzm.cloudfront.net'
+  @cloudfront = ''
+  slim :'chapters/chapter0'
 end
