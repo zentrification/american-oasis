01e92a428ca8b8e3bd8905a6d824c381c420db9c
Chris Dahms <zentrification@gmail.com>
2013-07-05 20:29:12 -0400

    switched back to cloudfront

--- app.rb
+++ app.rb
@@ -1,15 +1,13 @@
 require 'sinatra'
 
 get '/chapters/:page' do
-  @cloudfront = 'http://d2sc30funx5qzm.cloudfront.net'
-  @cloudfront = ''
+  @cloudfront = 'http://assets.american-oasis.com.s3-website-us-east-1.amazonaws.com/public'
   @page = params[:page]
   file_location = 'chapters/chapter' + @page
   slim file_location.to_sym
 end
 
 get '/*' do
-  @cloudfront = 'http://d2sc30funx5qzm.cloudfront.net'
-  @cloudfront = ''
+  @cloudfront = 'http://assets.american-oasis.com.s3-website-us-east-1.amazonaws.com/public'
   slim :'chapters/chapter0'
 end
